d6ed9c908c7106fa860ac4961a1b545b95c3873f
陈某人 <136078478+2321002853@users.noreply.github.com>
2023-08-10 16:25:57 +0800

    updata in testclass

--- src/test/java/com/itheima/Mybatisplus01QuickstartApplicationTests.java
+++ src/test/java/com/itheima/Mybatisplus01QuickstartApplicationTests.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
-
+// 测试pull
 @SpringBootTest
 class Mybatisplus01QuickstartApplicationTests {
 
